cdc62102a4ba994c9eca1832ee8f665776dba8c6
Lars Alexander Blumberg <lars.blumberg@posteo.de>
2015-12-11 14:40:53 +0100

    Add Xcode project and workspace files along with Pod dependencies

---
+++ Nuimo/Nuimo.h
@@ -0,0 +1,19 @@
+//
+//  Nuimo.h
+//  Nuimo
+//
+//  Created by Lars Blumberg on 12/11/15.
+//  Copyright © 2015 senic. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Nuimo.
+FOUNDATION_EXPORT double NuimoVersionNumber;
+
+//! Project version string for Nuimo.
+FOUNDATION_EXPORT const unsigned char NuimoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Nuimo/PublicHeader.h>
+
+
---
+++ Pods/Target Support Files/SwiftWebSocket/SwiftWebSocket-umbrella.h
@@ -0,0 +1,6 @@
+#import <Cocoa/Cocoa.h>
+
+
+FOUNDATION_EXPORT double SwiftWebSocketVersionNumber;
+FOUNDATION_EXPORT const unsigned char SwiftWebSocketVersionString[];
+
